0d3aec5a2898cb2dc91c886c2978567eef674689
coder8479 <peter.kuehnel@mpknails.de>
2024-04-06 09:07:43 +0200

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '624cefd16dd4622d790caa409389eaa8494399af',
+        'name' => 'damann8479/address',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'damann8479/address' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '624cefd16dd4622d790caa409389eaa8494399af',
+            'dev_requirement' => false,
+        ),
+    ),
+);
